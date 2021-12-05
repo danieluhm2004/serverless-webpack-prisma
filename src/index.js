cb9804293276baf7d7a1831fe112637120bd1239
Plus Pingya <plus.pingya@invenco.net>
2021-12-06 10:01:03 +1100

    Revert "Hard code function name to "service""
    This reverts commit 198e6a9a85e8d9ad6679f0c96be6cc4ea0d82bba.

--- src/index.js
+++ src/index.js
@@ -56,8 +56,26 @@ class ServerlessWebpackPrisma {
     }
   }
 
+  // Ref: https://github.com/serverless-heaven/serverless-webpack/blob/4785eb5e5520c0ce909b8270e5338ef49fab678e/lib/utils.js#L115)
   getFunctions() {
-    return ['service'];
+    const functions = this.serverless.service.getAllFunctions();
+
+    return functions.filter((funcName) => {
+      const func = this.serverless.service.getFunction(funcName);
+
+      // if `uri` is provided or simple remote image path, it means the
+      // image isn't built by Serverless so we shouldn't take care of it
+      if (
+        (func.image && func.image.uri) ||
+        (func.image && typeof func.image == 'string')
+      ) {
+        return false;
+      }
+
+      return this.isNodeRuntime(
+        func.runtime || this.serverless.service.provider.runtime || 'nodejs'
+      );
+    });
   }
 
   isNodeRuntime(runtime) {
